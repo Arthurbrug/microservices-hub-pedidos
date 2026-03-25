6e1ebfddc459fa30d5fb81243449d554898263f3
arthurbrug <arthurbrugnerottocerqueira@gmail.com>
2026-03-25 20:38:43 -0300

    Criação da pasta Entities (Pedido, ItemDoPedido) e implentando application-test.properties

---
+++ ms-pedidos/src/main/java/com/github/arthurbrug/ms/pedidos/entities/ItemDoPedido.java
@@ -0,0 +1,27 @@
+package com.github.arthurbrug.ms.pedidos.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_item_do_pedido")
+public class ItemDoPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private Integer quantidade;
+    @Column(nullable = false)
+    private String descricao;
+    @Column(nullable = false)
+    private BigDecimal precoUnitario;
+}
