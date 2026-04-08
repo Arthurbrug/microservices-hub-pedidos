e7644b2f9c1af53819c80defa5454313610e6ecb
ARTHURBRUG <arthurbrugnerottocerqueira@gmail.com>
2026-04-08 19:58:02 -0300

    iMPLEMENTANDO import.sql E PASTA EXCEPTION

---
+++ ms-pagamentos/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_pagamento(valor, nome, numero_cartao, validade, codigo_seguranca, status, pedido_id)
+VALUES (790.00, 'JON SNOW', '6894365478932145', '12/30', '589', 'CRIADO', 1);
+
+INSERT INTO tb_pagamento(valor, nome, numero_cartao, validade, codigo_seguranca, status, pedido_id)
+VALUES (3599.0, 'AYRA STARK', '1234567812345678', '01/28', '389', 'CRIADO', 2);
